5125a4ca42670f5afbe94b3c8e6d432fe271b1e1
Trần Giang <giangtv@finalstyle.com>
2022-05-08 16:34:47 +0700

    #

--- source/blocks/news/views/latest.php
+++ source/blocks/news/views/latest.php
@@ -18,7 +18,17 @@ $Itemid = 5;
                                 <a class="thumb" href="<?php echo $link;?>" title="<?php echo $title;?>">
                                     <img class="img-fluid" src="<?php echo URL_ROOT.str_replace('/original/','/small/', $item->image); ?>" alt="<?php echo $title;?>" />
                                 </a>
-                                <h4 class="heading"><a href="<?php echo $link;?>" title="<?php echo $title ?>"><?php echo $item->title?></a></h4>
+                                <h4 class="heading">
+                                    <a href="<?php echo $link;?>" title="<?php echo $title ?>"><?php echo $item->title?></a>
+                                    <span class="date">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-calendar4-week" viewBox="0 0 16 16">
+                                            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1H2zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5z"/>
+                                            <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+                                        </svg>
+                                        <?php echo date('M d', strtotime($item->created_time)); ?>
+                                    </span>
+                                </h4>
+                                <p><?php echo $item->summary?></p>
                             </div>
                         </article><!-- /.post-item-->
                     <?php }?>
@@ -34,7 +44,16 @@ $Itemid = 5;
                             <a class="thumb" href="<?php echo $link;?>" title="<?php echo $title;?>">
                                 <img class="img-fluid" src="<?php echo URL_ROOT.str_replace('/original/','/small/', $item->image); ?>" alt="<?php echo $title;?>" />
                             </a>
-                            <h4 class="heading"><a href="<?php echo $link;?>" title="<?php echo $title ?>"><?php echo $item->title?></a></h4>
+                            <h4 class="heading">
+                                <a href="<?php echo $link;?>" title="<?php echo $title ?>"><?php echo $item->title?></a>
+                                <span class="date">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-calendar4-week" viewBox="0 0 16 16">
+                                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1H2zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5z"/>
+                                        <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+                                    </svg>
+                                    <?php echo date('M d', strtotime($item->created_time)); ?>
+                                </span>
+                            </h4>
                         </article><!-- /.post-item-->
                     <?php $i++; }?>
                 </div>
--- source/templates/default/index.php
+++ source/templates/default/index.php
@@ -39,8 +39,13 @@ $tmpl->addScript('slick.min', '', 'top');
             <div class="inputSearchWrapper">
                 <?php $link = '/search'; ?>
                 <form action="<?php echo $link ?>" onsubmit="return submitSearch();">
-                    <input id="keyword" name="keyword" type="text" placeholder="Search" />
+                    <input id="keyword" name="keyword" type="text" placeholder="Search..." />
                     <input type="hidden" id="link_search" value="<?php echo $link ?>" />
+                    <button type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </button>
                 </form>
             </div>
             <div class="txtTrending">
